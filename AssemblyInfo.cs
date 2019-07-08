d961c942ee996a945233c331462deef9946e1dd5
Herp Derpinstine <herpderpinstine@gmail.com>
2019-07-07 20:09:07 -0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(QuitFix.ModInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(QuitFix.ModInfo.Company)]
+[assembly: AssemblyProduct(QuitFix.ModInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + QuitFix.ModInfo.Author)]
+[assembly: AssemblyTrademark(QuitFix.ModInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3a25f309-f002-4b35-a6f0-6b31f9a9c362")]
+[assembly: AssemblyVersion(QuitFix.ModInfo.Version)]
+[assembly: AssemblyFileVersion(QuitFix.ModInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
---
+++ QuitFix.cs
@@ -0,0 +1,26 @@
+﻿using VRCModLoader;
+using System.Diagnostics;
+
+namespace QuitFix
+{
+    public static class ModInfo
+    {
+        public const string Name = "QuitFix";
+        public const string Author = "Herp Derpinstine";
+        public const string Company = "NanoNuke @ nanonuke.net";
+        public const string Version = "1.0.0.0";
+    }
+    [VRCModInfo(ModInfo.Name, ModInfo.Version, ModInfo.Author)]
+
+    public class QuitFix : VRCMod
+    {
+        void OnApplicationStart(){}
+        void OnApplicationQuit() { Process.GetCurrentProcess().Kill(); }
+        void OnLevelWasLoaded(int level) {}
+        void OnLevelWasInitialized(int level) {}
+        void OnUpdate() {}
+        void OnFixedUpdate() {}
+        void OnLateUpdate() {}
+        void OnGUI() {}
+    }
+}
