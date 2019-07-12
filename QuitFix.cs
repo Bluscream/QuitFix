eb8c30fe056b89113e072d7a1bc60a6e9eb955b9
Herp Derpinstine <herpderpinstine@gmail.com>
2019-07-11 21:35:29 -0500

    Fixed execution order

--- QuitFix.cs
+++ QuitFix.cs
@@ -1,5 +1,6 @@
 ﻿using VRCModLoader;
 using System.Diagnostics;
+using System.Collections;
 
 namespace QuitFix
 {
@@ -8,19 +9,20 @@ namespace QuitFix
         public const string Name = "QuitFix";
         public const string Author = "Herp Derpinstine";
         public const string Company = "NanoNuke @ nanonuke.net";
-        public const string Version = "1.0.0.0";
+        public const string Version = "1.0.0.1";
     }
     [VRCModInfo(ModInfo.Name, ModInfo.Version, ModInfo.Author)]
 
     public class QuitFix : VRCMod
     {
-        void OnApplicationStart(){}
-        void OnApplicationQuit() { Process.GetCurrentProcess().Kill(); }
+        void OnApplicationStart() {}
+        void OnApplicationQuit() { ModManager.StartCoroutine(WaitForQuit()); }
         void OnLevelWasLoaded(int level) {}
         void OnLevelWasInitialized(int level) {}
         void OnUpdate() {}
         void OnFixedUpdate() {}
         void OnLateUpdate() {}
         void OnGUI() {}
+        public static IEnumerator WaitForQuit() { yield return 0; Process.GetCurrentProcess().Kill(); }
     }
 }
